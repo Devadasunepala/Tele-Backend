cc07c2cd1732cb5ca987d56cb99a451a83be920c
Devadasunepala <devadasunepala43@gmail.com>
2024-11-16 20:06:30 +0530

    changed

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-router.post('/signup', async (req, res) => {
-  const { fullName, email, phoneNumber } = req.body;
+// router.post('/signup', async (req, res) => {
+//   const { fullName, email, phoneNumber } = req.body;
    
-   try {
-      const existingUser = await User.findOne({ email });
-      if (existingUser) return res.status(400).json({ message: "User already exists" });
-      const newUser = new User({ fullName, email, phoneNumber });
-      await newUser.save();
-      res.status(201).json({ message: "submitted successfully" });
-    } 
-   catch (error) {
-      console.error(error.stack);
-      res.status(500).json({ message: "Server error" });
-  }
-});
+//    try {
+//       const existingUser = await User.findOne({ email });
+//       if (existingUser) return res.status(400).json({ message: "User already exists" });
+//       const newUser = new User({ fullName, email, phoneNumber });
+//       await newUser.save();
+//       res.status(201).json({ message: "submitted successfully" });
+//     } 
+//    catch (error) {
+//       console.error(error.stack);
+//       res.status(500).json({ message: "Server error" });
+//   }
+// });
 module.exports = router;
--- server.js
+++ server.js
@@ -2,14 +2,29 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/auth');
+//const authRoutes = require('./routes/auth');
 const cors = require('cors');
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api', authRoutes);
+//app.use('/api', authRoutes);
 const PORT = process.env.PORT || 5000;
+app.post('/signup', async (req, res) => {
+  const { fullName, email, phoneNumber } = req.body;
+   
+   try {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) return res.status(400).json({ message: "User already exists" });
+      const newUser = new User({ fullName, email, phoneNumber });
+      await newUser.save();
+      res.status(201).json({ message: "submitted successfully" });
+    } 
+   catch (error) {
+      console.error(error.stack);
+      res.status(500).json({ message: "Server error" });
+  }
+});
 app.use('/',async (req,res)=>{
   res.send('welcome to first project')
 })
