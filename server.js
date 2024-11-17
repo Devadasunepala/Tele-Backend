f54d1d833003089e091ece6f5306f79fedda73b8
Devadasunepala <devadasunepala43@gmail.com>
2024-11-17 11:16:20 +0530

    connection changed

--- server.js
+++ server.js
@@ -11,12 +11,18 @@ app.use(bodyParser.json());
 app.use('/api', authRoutes);
 const PORT = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-});
+// mongoose.connect(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// }).then(() => {
+//   console.log('Connected to MongoDB');
+// }).catch((error) => {
+//   console.error('Error connecting to MongoDB:', error);
+// });
+mongoose.connect(uri)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+    console.error('Ensure the IP is whitelisted and URI is correct.');
+  });
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
