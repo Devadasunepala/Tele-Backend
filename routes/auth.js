b6c6a3382b7bd56ab8fea6ead814b2397be44245
Devadasunepala <devadasunepala43@gmail.com>
2024-11-21 20:57:52 +0530

    response changed

--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,10 @@ router.post('/signup', async (req, res) => {
   const { fullName, email, phoneNumber } = req.body;
    try {
       const existingUser = await User.findOne({ email });
-      if (existingUser) return res.status(400).json({ message: "User already exists" });
+      if (existingUser) return res.status(400).json({ message: "Details Already Submittted. Thank you!" });
       const newUser = new User({ fullName, email, phoneNumber });
       await newUser.save();
-      res.status(201).json({ message: "submitted successfully" });
+      res.status(201).json({ message: "Submitted Successfully! We Will Respond To You Shortly" });
     } 
    catch (error) {
       console.error(error.stack);
